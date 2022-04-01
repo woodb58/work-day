a0d659b3a8255a462c7511989e885526225e1d9b
woodb58 <woodb58@gmail.com>
2022-03-31 23:03:29 -0500

    HTML time blocks added

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
-// var rightNow = moment().format("dddd, MMMM Do");
-
-// var Time = function () {
-//   $("#currentDay").append(rightNow);
-// };
-// Time();
-
-var currentDay = moment().format("dddd, MMMM Do");
-$("#currentDay").text(currentDay);
+var updateTime = function () {
+  document.getElementById("currentDay").innerHTML = moment().format(
+    "MMMM Do YYYY, h:mm:ss a"
+  );
+};
+setInterval(updateTime, 1000);
