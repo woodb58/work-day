24d5959d1d6612ee687a94d814e591f2fa0c7b9c
woodb58 <woodb58@gmail.com>
2022-04-02 15:33:25 -0500

    button save to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
-var updateTime = function () {
-  document.getElementById("currentDay").innerHTML = moment().format(
-    "MMMM Do YYYY, h:mm:ss a"
-  );
-};
-setInterval(updateTime, 1000);
+var currentDay = moment().format("MMMM Do YYYY");
+var currentHour = moment().format("HH:mm a");
+
+$(document).ready(function () {
+  var getTime = document.getElementById("currentDay");
+  getTime.innerHTML = currentDay;
+});
+
+$(".saveBtn").on("click", function () {
+  // Get nearby values in JQuery
+  var text = $(this).siblings(".col-md-10").val();
+  var time = $(this).parent().attr("id");
+
+  // Save text in local storage
+  localStorage.setItem(time, text);
+});
