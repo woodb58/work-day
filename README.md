15977e7b852251148e8391cc112946c5e5f80e14
woodb58 <woodb58@gmail.com>
2022-04-02 20:40:49 -0500

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# work-day
+# Third-Party APIs Challenge: Work Day Scheduler
+
+-When the planner is opened the current day of the week, date and time is displayed
+
+-When the user scrolls down they are presented with time blocks for standard business hours
+
+-Each time block is color-coded to indicate whether it is in the past, present, or future
+
+-When the user clicks a time block they can enter an event
+
+-When the user clicks the save button for that time block the text for that event is saved in local storage
+
+-When then user refreshes the page the saved events persist
+
+[Site Link] https://woodb58.github.io/work-day/
+
+![Portfolio Layout](./assets/images/screenshot.png)
+
+### Credits
+
+Font Awesome Icons -https://fontawesome.com/
--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,22 @@ var currentDayWeek = moment().format("dddd");
 var currentDate = moment().format("MMMM Do");
 var currentHour = moment().format("h:mm a");
 
+// date and time for header
 $(document).ready(function () {
   var getTime = document.getElementById("currentDay");
   getTime.innerHTML = currentDayWeek + ", " + currentDate + " " + currentHour;
 });
 
+// button to save to local storage
 $(".saveBtn").on("click", function () {
-  // Get nearby values of the description in JQuery
-  var text = $(this).siblings(".description").val();
+  // Get nearby values of the userText in JQuery
+  var text = $(this).siblings(".userText").val();
   var time = $(this).parent().attr("id");
-
-  // Save text in local storage
   localStorage.setItem(time, text);
 });
 
 function setClass() {
-  //get current number of hours.
+  //get current hour
   var currentTime = moment().hour();
 
   // loop over time blocks
@@ -40,20 +40,21 @@ function setClass() {
     }
   });
 }
-
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+// retrieve from local storage
+$("#hour8 .userText").val(localStorage.getItem("hour8"));
+$("#hour9 .userText").val(localStorage.getItem("hour9"));
+$("#hour10 .userText").val(localStorage.getItem("hour10"));
+$("#hour11 .userText").val(localStorage.getItem("hour11"));
+$("#hour12 .userText").val(localStorage.getItem("hour12"));
+$("#hour13 .userText").val(localStorage.getItem("hour13"));
+$("#hour14 .userText").val(localStorage.getItem("hour14"));
+$("#hour15 .userText").val(localStorage.getItem("hour15"));
+$("#hour16 .userText").val(localStorage.getItem("hour16"));
+$("#hour17 .userText").val(localStorage.getItem("hour17"));
 
 setClass();
 
+// button clears local storage and text area
 $("#clearBtn").click(function (event) {
   event.preventDefault;
   $("textarea").val("");
